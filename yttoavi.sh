4d04a6936df4499776cdde645d10ebda59e132fa
nardev <vedran@nardev.org>
2012-06-23 21:03:45 +0200

    init yt.sh

--- yttoavi.sh
+++ yttoavi.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 # 20-30 vids are maximum, depends on your internet connection and processor power
+
+while read p; do
+  youtube-dl -t $p &
+done < linkovi
+wait
+
+for f in *.flv
+do
+ffmpeg -i $f -vcodec mpeg4 -target pal-vcd -r 25 -s 480x234 $f.avi &
+done
+wait
+
+echo "DONE...............";
